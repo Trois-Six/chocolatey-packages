39baafe7277f5e2b045b1241532693e1515e38df
Pierre Erraud <pierre.erraud@cdiscount.com>
2015-12-28 17:12:33 +0100

    Add IZArc package

---
+++ izarc.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName   = 'izarc'
+  fileType      = 'EXE'
+  url           = 'http://www.izarc.org/download/IZArc_4.2.exe'
+  silentArgs   = '/verysilent'
+  validExitCodes= @(0)
+  checksum      = '1c8674752b60e8e5861938736fd3d425'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
